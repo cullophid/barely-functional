f1d72c45cb38db99f7bafc43f2f31a9eebd71b5e
stefano <svozza@gmail.com>
2015-12-23 19:51:10 +0000

    add merge function

---
+++ test/merge.js
@@ -0,0 +1,18 @@
+const assert = require('assert')
+const bf = require('../index.js')
+
+describe('assign', () => {
+
+    it('should merge two objects', () => {
+        const obj = {a: 1}
+        const obj1 = {b: 2}
+        assert.deepEqual(bf.merge(obj, obj1), {a: 1, b: 2})
+    })
+
+    it('merges objects and duplicate keys are resolved as last in wins', () => {
+        const obj = {a: 1, b:1}
+        const obj1 = {b: 2}
+        assert.deepEqual(bf.merge(obj, obj1), {a: 1, b: 2})
+    })
+
+})
