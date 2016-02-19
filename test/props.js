e86ead739f553df8e8fed3412631a013d8465e4a
Stefano Vozza <svozza@gmail.com>
2016-02-19 15:08:52 +0000

    modularise prop and props functions

--- test/props.js
+++ test/props.js
@@ -1,9 +1,20 @@
 const assert = require('assert')
+const props = require('../props.js')
 const bf = require('../index.js')
 
 describe('props', () => {
 
+    it('should be curried', () => {
+        const obj = {a: 1, b: 2, c: 3}
+        assert.deepEqual(props(['a', 'b'])(obj), [1, 2])
+    })
+
     it('should return the specified values from an object', () => {
+        const obj = {a: 1, b: 2, c: 3}
+        assert.deepEqual(props(['a', 'b'], obj), [1, 2])
+    })
+
+    it('should be exported from index', () => {
         const obj = {a: 1, b: 2, c: 3}
         assert.deepEqual(bf.props(['a', 'b'], obj), [1, 2])
     })
