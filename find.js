37e053cb8b15830e7fe386cf3ed4f6ca7cc9f15c
Stefano Vozza <svozza@gmail.com>
2016-03-17 21:38:07 +0000

    make array index functions safe

--- find.js
+++ find.js
@@ -1,3 +1,8 @@
+const Nothing = require('./Maybe').Nothing
+const Just = require('./Maybe').Just
 const curry = require('./curry')
 
-module.exports = curry((pred, xs) => xs.find(pred))
+module.exports = curry((pred, xs) => {
+    const found = xs.find(pred)
+    return found == null ? Nothing() : Just(found)
+})
--- nth.js
+++ nth.js
@@ -1,3 +1,5 @@
 const curry = require('./curry')
+const Nothing = require('./Maybe').Nothing
+const Just = require('./Maybe').Just
 
-module.exports = curry((i, xs) => xs[i])
+module.exports = curry((i, xs) => i >= xs.length ? Nothing() : Just(xs[i]))
